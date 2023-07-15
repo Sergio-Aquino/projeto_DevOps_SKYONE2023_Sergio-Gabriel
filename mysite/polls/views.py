ccb901bb817099e51004eba8f9e69459554159cc
Sergio <sergioadm120@gmail.com>
2023-07-15 12:36:02 -0300

    refract(polls): Refract views and URLs in order to use class-based views and generic templatess

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,20 +1,25 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -35,8 +40,6 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-
-
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
